f3f28026786a8dd515135e95e4caf1e0821f45b1
Tiago Santi <tiagosanti2009@gmail.com>
2023-07-18 14:41:20 -0400

    refactor(main.py): remove o endpoint de upload de dataset

--- app/main.py
+++ app/main.py
@@ -2,8 +2,6 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.openapi.utils import get_openapi
-import pandas as pd
-from io import StringIO
 
 app = FastAPI()
 
@@ -35,30 +33,6 @@ def hello():
     Este endpoint retorna uma mensagem de boas vindas
     """
     return JSONResponse(content={'message': '4Banks API!'})
-
-@app.post("/uploadcsv/")
-async def create_upload_file(file: UploadFile = File(...)):
-    '''
-    Esta função lida com solicitações POST para carregar um arquivo CSV.
-    
-    Parâmetros: 
-        file (UploadFile, obrigatório): Um arquivo CSV para upload. O campo deve ser incluído no corpo da solicitação POST.
-                                        O arquivo CSV é lido e decodificado em um DataFrame pandas.
-    
-    Retorna: 
-        dict: Um dicionário contendo:
-            filename (str): O nome do arquivo carregado.
-            rows (int): O número de linhas no arquivo CSV carregado.
-            columns (int): O número de colunas no arquivo CSV carregado.
-    '''
-    contents = await file.read()
-    df = pd.read_csv(StringIO(contents.decode('utf-8')))
-
-    return {
-        "filename": file.filename,
-        "rows": df.shape[0],
-        "columns": df.shape[1]
-    }
 
 def custom_openapi():
     if app.openapi_schema:
