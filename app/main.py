ad7ed23bd47d395f74044e7d61fb910c0510e86b
Tiago Santi <tiagosanti2009@gmail.com>
2023-07-28 09:41:12 -0400

    feat(api_acess): adiciona http://34.172.61.208:3000/ como origem permitida

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:3000'],
+    allow_origins=['http://localhost:3000', 'http://34.172.61.208:3000'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
